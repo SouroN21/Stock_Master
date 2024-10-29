4d297626dfb70aa0639fefd5556285a728d7ea0e
SouroN21 <dasunpriyanveen@gmail.com>
2024-10-29 14:43:31 +0530

    pots

--- StockMaster/backend/index.js
+++ StockMaster/backend/index.js
@@ -10,7 +10,7 @@ const productRoutes = require("./src/routes/productRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json()); 
