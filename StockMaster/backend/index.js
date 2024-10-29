a1d669b51a13cae1e73043a585448b81f7ec15ea
SouroN21 <dasunpriyanveen@gmail.com>
2024-10-29 13:30:13 +0530

    fix 3

--- StockMaster/backend/index.js
+++ StockMaster/backend/index.js
@@ -10,7 +10,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
+const PORT =  8080;
+
 
 app.use(cors());
 app.use(express.json()); 
